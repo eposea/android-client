37fae6d568551de119b870addbf2bcb67d5ba0ba
savvasenok <nightowlsavva@gmail.com>
2022-02-07 13:18:39 +0200

    Added navigation, new layouts for loading and errors, changed app colors

--- app/src/main/java/xyz/savvamirzoyan/eposea/ui/activity/MainActivity.kt
+++ app/src/main/java/xyz/savvamirzoyan/eposea/ui/activity/MainActivity.kt
@@ -2,11 +2,28 @@ package xyz.savvamirzoyan.eposea.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import xyz.savvamirzoyan.eposea.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy { findNavController(R.id.main_nav_host_fragment) }
+    private val appBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationUI.setupWithNavController(
+            findViewById<NavigationView>(R.id.navigation_view),
+            navController
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
